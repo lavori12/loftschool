01b86bb43ce6a79a64b222840329cfd90d85fc93
Lena Furta <lenyce4ka@gmail.com>
2019-05-28 21:19:43 +0300

    finished tasks

--- src/index.js
+++ src/index.js
@@ -17,6 +17,21 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
+    if ((array.length === 0) || !(array instanceof Array)) {
+        throw new Error ('empty array');
+    }
+    if (typeof fn != 'function') {
+        throw new Error ('fn is not a function');
+    }
+    let flag = true;
+
+    for (let i = 0; i < array.length; i++) {
+        if (fn(array[i]) !== true) {
+            flag = false;
+        }
+    }
+
+    return flag;
 }
 
 /*
@@ -36,6 +51,21 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
+    if ((array.length === 0) || !(array instanceof Array)) {
+        throw new Error ('empty array');
+    }
+    if (typeof fn != 'function') {
+        throw new Error ('fn is not a function');
+    }
+    let flag = false;
+
+    for (let i = 0; i < array.length; i++) {
+        if (fn(array[i]) === true) {
+            flag = true;
+        }
+    }
+
+    return flag;
 }
 
 /*
@@ -50,6 +80,20 @@ function isSomeTrue(array, fn) {
    - fn не является функцией (с текстом "fn is not a function")
  */
 function returnBadArguments(fn) {
+    if (typeof fn != 'function') {
+        throw new Error ('fn is not a function');
+    }
+    let badArguments = [];
+
+    for (let i = 1; i < arguments.length; i++) {
+        try {
+            fn(arguments[i]);
+        } catch (e) {
+            badArguments.push(arguments[i]);
+        }
+    }
+
+    return badArguments;
 }
 
 /*
@@ -69,7 +113,38 @@ function returnBadArguments(fn) {
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator() {
+function calculator(number = 0) {
+    if (typeof (number) != 'number') {
+        throw new Error ('number is not a number');
+    }
+
+    return {
+        sum: function() {
+            Array.from(arguments).forEach((element)=>number += element);
+
+            return number;
+        },
+        dif: function() {
+            Array.from(arguments).forEach((element)=>number -= element);
+
+            return number;
+        },
+        div: function() {
+            Array.from(arguments).forEach((element)=>{
+                if (element === 0) {
+                    throw new Error ('division by 0');
+                }
+                number /= element;
+            });
+
+            return number;
+        },
+        mul: function() {
+            Array.from(arguments).forEach((element)=>number *= element);
+
+            return number;
+        },
+    };
 }
 
 /* При решении задач, пострайтесь использовать отладчик */
