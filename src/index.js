dd8f16d5f9a809982f2527918a9e00b6511999b7
Lena Furta <lenyce4ka@gmail.com>
2019-06-23 22:33:53 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-var map;
+google.maps.event.addDomListener(window, 'load', initMap);
+
+const myMap = new google.maps.Map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    myMap = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: -34.397,
             lng: 150.644
