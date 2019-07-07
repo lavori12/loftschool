d6af8e479da65faeceedde556ce37175bbc776e6
Lena Furta <lenyce4ka@gmail.com>
2019-07-07 19:12:13 +0300

    fininshed chat

--- src/app.js
+++ src/app.js
@@ -1,37 +1,357 @@
-const authWindow = document.getElementById('authWindow'),
-    loginButton = document.getElementById('loginButton'),
-    uploadButton = document.getElementById('uploadButton'),
-    cancelButton = document.getElementById('cancelButton'),
-    fileElem = document.getElementById('fileElem'),
-    allMembers = document.getElementById('members'),
-    counter = document.getElementById('counter'),
-    avatar = document.getElementById('avatar'),
-    textMessage = document.getElementById('textMessage'),
-    sendButton = document.getElementById('sendButton'),
-    messageContainer = document.getElementById('messagesContainer'),
+const authPopup = document.getElementById('authPopup'),
+    authForm = document.getElementById('authForm'),
     userNameInput = document.getElementById('userNameInput'),
     userLoginInput = document.getElementById('userLoginInput'),
-    userPhoto = document.getElementById('userPhoto'),
-    userName = document.getElementById('userName'),
+    photoPopup = document.getElementById('photoPopup'),
+    uploadPhotoButton = document.getElementById('uploadPhotoButton'),
+    cancelPhotoButton = document.getElementById('cancelPhotoButton'),
+    fileInput = document.getElementById('fileInput'),
+    label = document.getElementById('label'),
+    membersContainer = document.getElementById('membersContainer'),
+    counterItem = document.getElementById('counter'),
+    currentUserNameItem = document.getElementById('currentUserName'),
+    currentUserPhotoItem = document.getElementById('currentUserPhoto'),
     welcome = document.getElementById('welcome'),
-    userInfo = document.getElementById('userInfo'),
-    fakeUpload = document.getElementById('fakeUpload'),
+    userInfoContainer = document.getElementById('userInfoContainer'),
+    fakeUploadLabel = document.getElementById('fakeUploadLabel'),
+    messageContainer = document.getElementById('messagesContainer'),
+    messageInput = document.getElementById('messageInput'),
+    messageForm = document.getElementById('messageForm'),
     socket = new WebSocket('ws://localhost:8081');
 
 const NO_PHOTO = 'images/NO.jpg';
 
-let fileBase;
+let fileBase, currentMessage;
 
 let users = {},
-    messages = {},
-    loginData = {
-        name: '',
-        login: ''
-    },
-    onlineCounter = 0;
+    membersList = [],
+    currentLogin = '',
+    currentUser = null;
+
+/*
+const uploadImg = (data) => {
+    document.querySelector('.downloadsPhoto').innerHTML += `<div><img src="${data.data}" /><span>${data.user}</span></div>`;
+};
+
+const renderPhoto = (photos = []) => {
+    if (photos.length) {
+        const container = document.querySelector('.downloadsPhoto');
+
+        photos.forEach(photo => {
+            container.innerHTML += `<div><img src="${photo.img}" /><span>${photo.user}</span></div>`
+        });
+    }
+};
+*/
+
+
+const createMemberItem = (user) => {
+    if (!user) return;
+
+    const item = document.createElement('div');
+    item.innerHTML = user.name;
+    membersContainer.appendChild(item);
+
+    user.memberItem = item;
+
+    return item;
+};
+
+const createMessageItem = (user, timestamp, text) => {
+    if (!user) return;
+
+    const item = document.createElement('div');
+    item.className = 'message';
+
+    if (user == currentUser)
+        item.classList.add('userMessage');
+
+    const photoItem = document.createElement('img');
+    photoItem.className = 'messagePhoto';
+    photoItem.src = user.photo || NO_PHOTO;
+
+    const dataContainer = document.createElement('div');
+    dataContainer.className = 'messageDataContainer';
+
+    const metaContainer = document.createElement('div');
+    metaContainer.className = 'messageMetaContainer';
+
+    const nameItem = document.createElement('div');
+    nameItem.className = 'messageName';
+    nameItem.innerHTML = user.name;
+
+    const dateItem = document.createElement('div');
+    const date = new Date(timestamp);
+    let day, month, hours, minutes;
+    day = date.getDate().toString();
+    if (day.length < 2) {
+        day = '0' + day;
+    }
+    month = (date.getMonth() +1).toString();
+    if (month.length < 2) {
+        month = '0' + month;
+    }
+    minutes = date.getMinutes().toString();
+    if (minutes.length < 2) {
+        minutes = '0' + minutes;
+    }
+    hours = date.getHours().toString();
+    if (hours.length < 2) {
+        hours = '0' + hours;
+    }
+    dateItem.className = 'messageDate';
+    dateItem.innerHTML =  hours + ':' + minutes  + ' ' + day + '.' + month;
+
+    const textItem = document.createElement('div');
+    textItem.className = 'messageText';
+    textItem.innerHTML = text;
+
+
+    item.appendChild(photoItem);
+    item.appendChild(dataContainer);
+
+    dataContainer.appendChild(metaContainer);
+    dataContainer.appendChild(textItem);
+
+    metaContainer.appendChild(nameItem);
+    metaContainer.appendChild(dateItem);
+
+    messageContainer.appendChild(item);
+
+    if (!user.messagesItems)
+        user.messagesItems = [];
+
+    const messagesItem = {
+        item: item,
+        photo: photoItem,
+        name: nameItem
+    };
+    user.messagesItems.push(messagesItem);
+
+    return messagesItem;
+};
+
+const updateOnlineCounter = () => {
+    counterItem.textContent = membersList.length;
+};
+
+
+const updateCurrentUserName = () => {
+    currentUserNameItem.textContent = currentUser.name;
+};
+
+const updateCurrentUserPhoto = () => {
+    currentUserPhotoItem.src = currentUser.photo || NO_PHOTO;
+};
+
+
+const updateMessagesName = (user) => {
+    if (!user || !user.messagesItems) return;
+
+    for (const key in user.messagesItems) {
+        user.messagesItems[key].name.textContent = user.name;
+    }
+};
+
+const updateMessagesPhoto = (user) => {
+    if (!user || !user.messagesItems) return;
+
+    for (const key in user.messagesItems) {
+        user.messagesItems[key].photo.src = user.photo || NO_PHOTO;
+    }
+};
+
+
+const updateMemberName = (user, name) => {
+    if (!user || user.name == name) return;
+
+    user.name = name;
+
+    if (user.memberItem)
+        user.memberItem.textContent = user.name;
+
+    if (user == currentUser)
+        updateCurrentUserName();
+
+    updateMessagesName(user);
+};
+
+const updateMemberPhoto = (user, photo) => {
+    if (!user || user.photo == photo) return;
+
+    user.photo = photo;
+
+    if (user == currentUser)
+        updateCurrentUserPhoto();
+
+    updateMessagesPhoto(user);
+};
+
+const onLogin = (data) => {
+    if (!data) return;
+
+    users = data.users || {};
+    membersList = data.membersList || [];
+
+    currentUser = users[currentLogin];
+
+    if (!currentUser) {
+        alert('current user not found');
+        return;
+    }
     
+    for (const key in membersList) {
+        createMemberItem( users[membersList[key]] );
+    }
+    updateOnlineCounter();
+
+    updateCurrentUserName();
+    updateCurrentUserPhoto();
+
+
+    for (const key in data.messages) {
+        const messageData = data.messages[key];
+        createMessageItem(users[messageData.login], messageData.timestamp, messageData.message);
+    }
+
+    authPopup.style.display = 'none';
+    welcome.style.display = 'none';
+    userInfoContainer.style.display = 'block';
+
+    currentUserPhotoItem.addEventListener('click', () => {
+        photoPopup.style.display = 'block';
+        fakeUploadLabel.style.display = '';
+    });
+};
+
+const onNewMember = (userData) => {
+
+    if (!membersList.includes(userData.login)) {
+        membersList.push(userData.login);
+
+        updateOnlineCounter();
+    }
+    
+    let user = users[userData.login];
+    
+    if (!user) {
+        user = userData;
+        users[userData.login] = user;
+    }
+
+    if (!user.memberItem)
+        createMemberItem(user);
+
+    updateMemberName(user, userData.name);
+    updateMemberPhoto(user, userData.photo);
+};
+
+const onUpdateName = (userData) => {
+    updateMemberName(users[userData.login], userData.name);
+};
+
+const onUpdatePhoto = (userData) => {
+    updateMemberPhoto(users[userData.login], userData.photo);
+};
+
+const onMemberLeft = (login) => {
+    const memberIndex = membersList.indexOf(login);
+
+    if (memberIndex != -1) {
+        membersList.splice(memberIndex, 1);
+        updateOnlineCounter();
+    }
+
+
+    const user = users[login];
+
+    if (user) {
+        membersContainer.removeChild(user.memberItem);
+        user.memberItem = null;
+
+        if (user == currentUser) {
+            socket.close();
+            alert('You left the chat');
+        }
+    }
+};
+
+const onUploadPhoto = (status) => {
+    if (status === 'ok')
+        updateMemberPhoto(currentUser, fileBase);
+
+    fileBase = '';
+
+    closePhotoPopup();
+};
+
+const onSendMessage = (data) => {
+
+    if (data) {
+        createMessageItem(currentUser, data.timestamp, currentMessage);
+    }
+
+    currentMessage = '';
+};
+
+const onNewMessage = (messageData) => {
+    if (messageData)
+        createMessageItem(users[messageData.login], messageData.timestamp, messageData.message);
+};
+
+const login = () => {
+    currentLogin = userLoginInput.value;
+
+    const data = JSON.stringify({ payload: 'login', data: { login: currentLogin, name: userNameInput.value } });
+    socket.send(data);
+};
+
+const closePhotoPopup = () => {
+    photoPopup.style.display = 'none';
+    label.style.backgroundImage = '';
+};
+
+const sendMessage = (message) => {
+    if (!message || !currentUser || currentMessage) return;
+
+    currentMessage = message;
+
+    const data = JSON.stringify({ payload: 'sendMessage', data: currentMessage });
+    socket.send(data);
+};
+
+const addPhoto = () => {
+    const file = fileInput.files[0];
+    if (file.size/1024 > 512) {
+        alert ('Размер файла должен быть не больше 512 Кб');
+    } else if (file.type !== 'image/jpeg') {
+        alert ('Файл должен быть в формате JPEG');
+    } else {
+        let reader = new FileReader();
+
+        reader.onloadend = function (e) {
+            const img = e.target.result;
+            const container = label;
+            fakeUploadLabel.style.display = 'none';
+            fileBase = img;
+            container.style.backgroundImage =  `url('${img}')`;
+        };
+
+        reader.readAsDataURL(file);
+    }
+};
+
+const uploadPhoto = () => {
+    if (fileBase) {
+        const data = JSON.stringify({ payload: 'uploadPhoto', data: fileBase });
+        socket.send(data);
+    } else {
+        alert('Вы не выбрали фото');
+    }
+};
+
 socket.onopen = function () {
-    authWindow.style.display = 'block';
+    authPopup.style.display = 'block';
 };
 
 socket.onclose = function (event) {
@@ -46,25 +366,37 @@ socket.onclose = function (event) {
 socket.onmessage = function (event) {
     const res = JSON.parse(event.data);
 
+
+    if (res.error) {
+        alert('ERROR: ['+ res.payload + '] ' + res.error);
+        return;
+    }
+
     switch (res.payload) {
-        case 'login':
-            onLogin(res.data);
-            break;
-        case 'userConnect':
-            onUserConnect(res.data);
-            break;
-        case 'userDisconnect':
-            onUserDisconnect(res.data);
-            break;
-        case 'uploadPhoto':
-            onUploadPhoto(res.data);
-            break;
-        case 'updatePhoto':
-            onUpdatePhoto(res.data);
-            break;
-        case 'send_message':
-            sendMessage(res.data);
-            break;
+    case 'login':
+        onLogin(res.data);
+        break;
+    case 'newMember':
+        onNewMember(res.data);
+        break;
+    case 'updateName':
+        onUpdateName(res.data);
+        break;
+    case 'updatePhoto':
+        onUpdatePhoto(res.data);
+        break;
+    case 'memberLeft':
+        onMemberLeft(res.data);
+        break;
+    case 'uploadPhoto':
+        onUploadPhoto(res.data);
+        break;
+    case 'sendMessage':
+        onSendMessage(res.data);
+        break;
+    case 'newMessage':
+        onNewMessage(res.data);
+        break;
     }
 };
 
@@ -72,166 +404,27 @@ socket.onerror = function (error) {
     alert('Ошибка ' + error.message);
 };
 
-loginButton.addEventListener('click', () => {
-    loginData.name = userNameInput.value;
-    loginData.login = userLoginInput.value;
-    const data = JSON.stringify({ payload: 'login', data: loginData });
-    socket.send(data);
+authForm.addEventListener('submit', (e) => {
+    login();
+
+    e.preventDefault();
+    return false;
 });
 
-textMessage.addEventListener('keypress', (e) => {
-    if (e.keyCode === 13 && textMessage.value !== '') {
-        const data = JSON.stringify({ payload: 'new_message', data: textMessage.value });
+messageForm.addEventListener('submit', (e) => {
 
-        textMessage.value = '';
-        socket.send(data);
+    if (currentUser) {
+        sendMessage(messageInput.value);
+        messageInput.value = '';
     }
+
+    e.preventDefault();
+    return false;
 });
 
-sendButton.addEventListener('click', () => {
-    if (textMessage.value !== '') {
-        const data = JSON.stringify({ payload: 'new_message', data: textMessage.value });
+fileInput.addEventListener('change', addPhoto);
 
-        textMessage.value = '';
-        socket.send(data);
-    }
-});
+uploadPhotoButton.addEventListener('click',  uploadPhoto);
 
-fileElem.addEventListener('change', () => {
-
-
-    const file = fileElem.files[0];
-    console.log(file);
-    let reader = new FileReader();
-
-    reader.onloadend = function (e) {
-        const img = e.target.result;
-        const container = document.querySelector('.label');
-        fakeUpload.style.display = 'none';
-        fileBase = img;
-        container.style.backgroundImage =  `url('${img}')`;
-    };
-
-    reader.readAsDataURL(file);
-});
-
-uploadButton.addEventListener('click',  () => {
-    if (fileBase) {
-        const data = JSON.stringify({ payload: 'uploadPhoto', data: fileBase });
-
-        socket.send(data);
-    } else {
-        alert('Вы не выбрали фото');
-    }
-});
-
-cancelButton.addEventListener('click', () => {
-    avatar.style.display = 'none';
-    document.querySelector('.label').style.backgroundImage = '';
-});
-
-const sendMessage = (data) => {
-    const newMessage = document.createElement('div'),
-        messagePhoto = document.createElement('img'),
-        messageName = document.createElement('span'),
-        messageText = document.createElement('span');
-
-    if (data.user && data.text) {
-        messageName.textContent = data.user.name + '    ';
-        messagePhoto.src = data.user.photo;
-        messagePhoto.width = 15 +'px';
-        messagePhoto.height = 15 +'px';
-        messagePhoto.style.borderRadius = '10px';
-        messageText.textContent = data.text;
-        messageName.style.color = '#ff9966';
-        newMessage.appendChild(messageName);
-        newMessage.appendChild(messageText);
-        messageContainer.appendChild(newMessage);
-    }
-};
-
-const uploadImg = (data) => {
-    document.querySelector('.downloadsPhoto').innerHTML += `<div><img src="${data.data}" /><span>${data.user}</span></div>`;
-};
-
-const renderPhoto = (photos = []) => {
-    if (photos.length) {
-        const container = document.querySelector('.downloadsPhoto');
-
-        photos.forEach(photo => {
-            container.innerHTML += `<div><img src="${photo.img}" /><span>${photo.user}</span></div>`
-        });
-    }
-};
-
-
-const onLogin = (data) => {
-    if (data) {
-        userName.innerHTML = `<h2>${loginData.name}</h2>`;
-        userPhoto.src = NO_PHOTO;
-        authWindow.style.display = 'none';
-        welcome.style.display = 'none';
-        userInfo.style.display = 'block';
-        userPhoto.addEventListener('click', () => {
-            avatar.style.display = 'block';
-            fakeUpload.style.display = '';
-        });
-
-        fillUsers(data.onlineUsers);
-        //updateMessages();
-    }
-};
-
-const createUser = (user) => {
-    const member = document.createElement('div');
-
-    member.innerHTML = user.name;
-    allMembers.appendChild(member);
-    users[user.login] = {
-        login: user.login,
-        name: user.name,
-        photo: user.photo || null,
-        element: member
-    };
-    updateOnlineCounter(1);
-};
-
-const onUserConnect = (data) => {
-
-    createUser(data);
-};
-
-const onUserDisconnect = (login) => {
-
-    allMembers.removeChild(users[login].element);
-    delete users[login];
-    updateOnlineCounter(-1);
-};
-
-const fillUsers = (onlineUsers) => {
-
-    for (const key in onlineUsers) {
-        createUser(onlineUsers[key]);
-    }
-};
-
-const updateOnlineCounter = (add) => {
-    onlineCounter+= add;
-    counter.textContent = `(${onlineCounter})`;
-};
-
-const onUploadPhoto = (status) => {
-    if (status === 'ok') {
-        userPhoto.src = fileBase;
-    }
-    updateMessagesPhoto(loginData.login, fileBase);
-};
-
-const onUpdatePhoto = (data) => {
-    updateMessagesPhoto(data.login, data.photo);
-};
-
-const updateMessagesPhoto = (login, photo) => {
-
-};
+cancelPhotoButton.addEventListener('click', closePhotoPopup);
 
--- src/server.js
+++ src/server.js
@@ -1,124 +1,271 @@
-let WebSocketServer = new require('ws'),
-    clients = {};
-
-const uuidv1 = require('uuid/v1'),
+const WebSocketServer = new require('ws'),
+    uuidv1 = require('uuid/v1'),
     low = require('lowdb'),
-    FileSync = require('lowdb/adapters/FileSync'),
-    adapter = new FileSync('database/db.json'),
+    FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('database/db.json'),
     db = low(adapter);
 
-db.defaults({ users: {}, messages: {} })
+/*
+* Сущности пользователей:
+*   user      - текущий пользователь, который сделал запрос
+*   member    - любой онлайн пользователь
+*   client    - подключившийся клиент (свяка между соединением и user)
+* */
+
+let clients = {}, 
+    members = {};
+
+const notifyMembers = (payload, data, excludeClient) => {
+
+    // у одного пользователя, может быть несколько соединений, а данные нужно актуализировать, поэтому пробегаемся по clients, а не по members
+    for (const key in clients) {
+        if (clients[key] !== excludeClient && clients[key].user) {
+            clients[key].connection.send( JSON.stringify({ payload: payload, data: data }) );
+        }
+    }
+};
+
+const userLogin = (client, userInfo) => {
+    if (!userInfo) {
+        throw new Error('no login data');
+    }
+
+    if (!userInfo.login) {
+        throw new Error('login is empty');
+    }
+
+    if (!userInfo.name) {
+        throw new Error('name is empty');
+    }
+
+    let membersNotification;
+
+    let user = members[userInfo.login];
+
+    if (user) {
+        user.connectionCount++;
+
+        if (user.name != userInfo.name) {
+            user.name = userInfo.name;
+            db.get(`users.${userInfo.login}`).assign({ name: userInfo.name }).write();
+
+            membersNotification = {
+                payload: 'updateName',
+                data: {
+                    login: userInfo.login,
+                    name: userInfo.name
+                }
+            };
+        }
+    } else {
+        // получаем пользователя и клонируем его, чтобы лишние данные не попали в базу
+        user = db.get(`users.${userInfo.login}`).value();
+
+        if (user) {
+            db.get(`users.${userInfo.login}`).assign({ name: userInfo.name }).write();
+        } else {
+            user = {
+                login: userInfo.login,
+                name: userInfo.name,
+                photo: null,
+                messages: []
+            };
+
+            db.set(`users.${userInfo.login}`, user).write();
+        }
+
+        user.connectionCount = 1;
+        members[userInfo.login] = user;
+
+        membersNotification = {
+            payload: 'newMember',
+            data: {
+                login: user.login,
+                name: user.name,
+                photo: user.photo
+            }
+        };
+    }
+
+    client.user = user;
+
+    let users = {}; // пользователи, нужные для отображения списка участников и сообщений
+    let membersList = [];
+
+    for (const key in members) {
+        const member = members[key];
+
+        membersList.push(member.login);
+
+        const userData = {
+            login: member.login,
+            name: member.name,
+            photo: member.photo
+        };
+
+        users[userData.login] = userData;
+    }
+
+    let messages = [];
+
+    for (const key in user.messages) {
+        const message = db.get('messages').find({ id: user.messages[key] }).value();
+
+        if (message) {
+            messages.push(message); 
+        }
+    }
+
+    return {
+        userResponseData: {
+            membersList: membersList,
+            users: users,
+            messages: messages
+        },
+
+        membersNotification: membersNotification
+    };
+};
+
+const uploadPhoto = (client, photo) => {
+    if (!client.user) {
+        throw new Error('user is not logged in'); 
+    }
+
+    if (!photo) {
+        throw new Error('no photo'); 
+    }
+
+    client.user.photo = photo;
+
+    db.get(`users.${client.user.login}`).assign({ photo: photo }).write();
+
+    return {
+        userResponseData: 'ok',
+        membersNotification: {
+            payload: 'updatePhoto',
+            data: {
+                login: client.user.login,
+                photo: client.user.photo
+            }
+        }
+    };
+};
+
+const sendMessage = (client, message) => {
+    if (!client.user) {
+        throw new Error('user is not logged in'); 
+    }
+
+    if (!message) {
+        throw new Error('no message'); 
+    }
+
+    const messageData = {
+        id: db.get('messages').itemCount().value(),
+        login: client.user.login,
+        timestamp: Date.now(),
+        message: message
+    };
+
+    db.get('messages').push(messageData).write();
+
+    // запоминаем сообщения, чтоб отобразить их при выходе
+    for (const key in members) {
+        db.get(`users.${key}`).get('messages').push(messageData.id).write();
+    }
+
+    return {
+        userResponseData: { id: messageData.id, timestamp: messageData.timestamp },
+        membersNotification: {
+            payload: 'newMessage',
+            data: messageData
+        }
+    };
+};
+
+db._.mixin({
+    itemCount: function(array) {
+        return array.length;
+    }
+});
+
+db.defaults({ users: {}, messages: [] })
     .write();
 
 let webSocketServer = new WebSocketServer.Server({
     port: 8081
 });
 
-webSocketServer.on('connection', function (ws) {
+webSocketServer.on('connection', function (connection) {
     let id = uuidv1();
 
     let client = {
         user: null,
-        connection: ws
+        connection: connection
     };
 
     clients[id] = client;
     console.log(' новое соединение ' + id);
 
-    ws.on('message', function (message) {
-        const data = JSON.parse(message);
+    connection.on('message', function (message) {
+        let payload;
 
-        switch (data.payload) {
+        try {
+            const data = JSON.parse(message);
+
+            // чтобы можно было отправить метод в ошибке
+            payload = data.payload;
+
+            let result;
+
+            switch (payload) {
             case 'login':
-                userLogin(client, data.data);
+                result = userLogin(client, data.data);
                 break;
             case 'uploadPhoto':
-                uploadPhoto(client, data.data);
+                result = uploadPhoto(client, data.data);
                 break;
-            case 'new_message':
-                sendMessage(client, data.data);
+            case 'sendMessage':
+                result = sendMessage(client, data.data);
                 break;
+            default:
+                throw new Error('Unknown method');
+            }
+
+            if (result) {
+
+                if (result.userResponseData) {
+                    connection.send( JSON.stringify({ payload: payload, data: result.userResponseData }) ); 
+                }
+
+                if (result.membersNotification) {
+                    notifyMembers(result.membersNotification.payload, result.membersNotification.data, client); 
+                }
+
+            }
+        } catch (error) {
+            connection.send( JSON.stringify({ payload: payload, error: error.message }) );
         }
 
     });
 
-    ws.on('close', function () {
-        console.log('соединение закрыто ' + id);
+    connection.on('close', function () {
+        console.log('соединение закрыто ' + id + ' :: ' + (client.user ? client.user.login: ''));
 
         delete clients[id];
-        for (const key in clients) {
-            const response = { payload: 'userDisconnect', data: client.user.login };
 
-            clients[key].connection.send(JSON.stringify(response));
+        if (client.user) {
+            client.user.connectionCount--;
+
+            if (client.user.connectionCount == 0) {
+                delete members[client.user.login];
+
+                notifyMembers('memberLeft', client.user.login);
+            }
         }
+
         client = null;
     });
 });
-
-const userLogin = (client, userInfo) => {
-
-
-    let user = db.get('users').find({login: userInfo.login}).value();
-
-
-    if (user) {
-        client.user = user;
-    } else {
-        client.user = {
-            login: userInfo.login,
-            photo: null
-        };
-
-        db.get('users').push({login: client.user.login}).write();
-        //db.set(`users.${client.user.login}`, {login: client.user.login}).write();
-    }
-    client.user.name = userInfo.name;
-
-    let onlineUsers = [];
-
-    for (const key in clients) {
-        let response;
-
-        onlineUsers.push(clients[key].user);
-
-        if(clients[key] !== client) {
-            response = { payload: 'userConnect', data: client.user };
-            if (clients[key].user) {
-                clients[key].connection.send(JSON.stringify(response));
-            }
-        }
-    }
-
-    let response = { payload: 'login', data: {onlineUsers: onlineUsers, messages: db.get('messages').value()} };
-    client.connection.send(JSON.stringify(response));
-};
-
-
-const sendMessage = (id, message) => {
-    for (const key in clients) {
-        const response = { payload: 'send_message', data: { user: clients[id], text: message } };
-
-        clients[key].connection.send(JSON.stringify(response));
-    }
-};
-
-const uploadPhoto = (client, photo) => {
-    if (client.user) {
-        client.user.photo = photo;
-        db.get('users').find({login: client.login}).assign({photo: photo}).write();
-
-        for (const key in clients) {
-            let response;
-
-            if(clients[key] !== client) {
-                response = { payload: 'updatePhoto', data: { login: client.user.login, photo: client.user.photo } };
-                if (clients[key].user) {
-                    clients[key].connection.send(JSON.stringify(response));
-                }
-            }
-        }
-
-        let response = { payload: 'uploadPhoto', data: 'ok' };
-        client.connection.send(JSON.stringify(response));
-    }
-};
